430c582597ec44361f292c3da79b2d018708b88e
Alan-Graton <graton.alan@gmail.com>
2024-01-01 22:43:39 -0300

    wip-fix: trying to fix `Login` screen with ScrollView. Still not enough

--- src/app/(login)/index.tsx
+++ src/app/(login)/index.tsx
@@ -9,6 +9,8 @@ import {
   Text,
   VStack,
   Box,
+  ScrollView,
+  InputIcon,
 } from "@gluestack-ui/themed";
 
 import { AppSafeAreaView } from "@/components/AppSafeAreaView";
@@ -46,75 +48,76 @@ export default function Login() {
         flexGrow: 1,
       }}
     >
-      <VStack
-        p="$12"
-        flex={1}
-        bg="$gray200"
-        borderBottomLeftRadius="$3xl"
-        borderBottomRightRadius="$3xl"
-        justifyContent="space-around"
-      >
-        <Center>
-          <Image
-            source={logo}
-            defaultSource={logo as ImageURISource}
-            w="$full"
-            resizeMode="contain"
-            alt="Marketspace Logo"
-          />
-          <Heading fontSize="$2xl">marketspace</Heading>
-          <Text>Seu espaço de compra e venda</Text>
-        </Center>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <VStack
+          p="$12"
+          bg="$gray200"
+          borderBottomLeftRadius="$3xl"
+          borderBottomRightRadius="$3xl"
+          justifyContent="space-around"
+        >
+          <Center>
+            <Image
+              source={logo}
+              defaultSource={logo as ImageURISource}
+              w="$full"
+              resizeMode="contain"
+              alt="Marketspace Logo"
+            />
+            <Heading fontSize="$2xl">marketspace</Heading>
+            <Text>Seu espaço de compra e venda</Text>
+          </Center>
 
-        <Center>
-          <Text>Acesse sua conta</Text>
-          <Controller
-            control={control}
-            name="email"
-            render={({ field: { onBlur, onChange, value } }) => (
-              <AppInput
-                placeholder="E-mail"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.email?.message}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="password"
-            render={({ field: { onBlur, onChange, value } }) => (
-              <AppInput
-                placeholder="Senha"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                errorMessage={errors.password?.message}
-              />
-            )}
-          />
+          <Center>
+            <Text>Acesse sua conta</Text>
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onBlur, onChange, value } }) => (
+                <AppInput
+                  placeholder="E-mail"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.email?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onBlur, onChange, value } }) => (
+                <AppInput
+                  placeholder="Senha"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
 
-          <AppButton
-            title="Entrar"
-            action="primary"
-            onPress={handleSubmit(onSubmit)}
-            mt="$7"
-          />
-        </Center>
-      </VStack>
-      <Box p="$12" justifyContent="center" bg="$gray100">
-        <Center>
-          <Text>Ainda não tem acesso?</Text>
-          <AppButton
-            title="Criar uma conta"
-            action="secondary"
-            onPress={() => {
-              router.push("/(signup)/");
-            }}
-          />
-        </Center>
-      </Box>
+            <AppButton
+              title="Entrar"
+              action="primary"
+              onPress={handleSubmit(onSubmit)}
+              mt="$7"
+            />
+          </Center>
+        </VStack>
+        <Box p="$12" justifyContent="center" bg="$gray100">
+          <Center>
+            <Text>Ainda não tem acesso?</Text>
+            <AppButton
+              title="Criar uma conta"
+              action="secondary"
+              onPress={() => {
+                router.push("/(signup)/");
+              }}
+            />
+          </Center>
+        </Box>
+      </ScrollView>
     </AppSafeAreaView>
   );
 }
